eac7bbef82445560451a1f47f3a625f0bb5d7a3d
tmurali <tmurali@costar.com>
2025-04-21 17:40:14 -0400

    second

--- utilities/insertion_deletion.go
+++ utilities/insertion_deletion.go
@@ -1,1 +1,14 @@
 package utilities
+
+// This function finds where a key fits inside a B-tree node (internal or leaf).
+// Specifically, it returns the index i of the largest key in the node that is less than or equal to the input key.
+func IndexToInsert(a TreeNode, keybytes []byte) uint16 {
+	no_of_keys := a.KeyCount()
+	for i: uint16(1);i<no_of_keys;i++ {
+		
+		if(comparison <= 0) {
+
+		}
+	}
+
+}
